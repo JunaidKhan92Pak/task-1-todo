6d3640b43063a7a83a53b858ecf163c43a11a536
Junaid Khan <113337547+JunaidKhan92Pak@users.noreply.github.com>
2024-01-20 21:01:06 +0500

    TODO LIST TASK 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Todo from './components/todo'
+import UserList from './components/userList'
+
+function App() {
+  return (
+    <>
+    <Todo/>
+    </>
+  )
+}
+
+export default App
